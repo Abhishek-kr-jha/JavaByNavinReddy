a3f5a1110c371d56e32ea595c777d26a08a1b4f2
abhishekkumarjha <abhishekkjha068@outlook.com>
2023-12-10 00:54:39 +0530

    Java Variables

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello{
+    public static void main(String a[]){
+        // variables in java
+       int  num = 3;
+       int num1 = 4;
+       int result = num+ num1;
+        System.out.println(result);
+    }
+
+}
