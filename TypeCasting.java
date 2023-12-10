b30d1490477a20f22c558a8a2e42e34e42f7f06e
Abhishek jha <Abhishekkjha068@outlook.com>
2023-12-10 13:09:17 +0530

    post nad prefix increment in java

--- TypeCasting.java
+++ TypeCasting.java
@@ -5,12 +5,22 @@ public class TypeCasting {
         // byte k = (byte)a;
         // float f = 5.6f;
         // int t =(int)f;
-        byte a = 10;
-        byte b = 30;
-        int result =a*b;
+        // byte a = 10;
+        // byte b = 30;
+        // int result =a*b;
 
+        int num = 7;
+        // imt num2 = 5;
+        // int result = num1%num2;
+        // num1 = num1+1;
+        // num+=1;
+        // num++; // post - increement
+        // ++num; // pre - increment
+        // num--;
+        int result = num++; // fetch the value and then increment
+        // int result = ++num // increment and then fetch the value;
 
         System.out.println(result);
     }
-    
+
 }
