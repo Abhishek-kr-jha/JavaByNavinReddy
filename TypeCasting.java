6e9ab8259fdda055bb826dc911641e83b49dfa4a
Abhishek jha <Abhishekkjha068@outlook.com>
2023-12-10 12:55:30 +0530

    conversion

--- TypeCasting.java
+++ TypeCasting.java
@@ -1,13 +1,16 @@
 public class TypeCasting {
     public static void main(String[] args) {
         // byte b = 125;
-        int a = 257;
-        byte k = (byte)a;
-        float f = 5.6f;
-        int t =(int)f;
+        // int a = 257;
+        // byte k = (byte)a;
+        // float f = 5.6f;
+        // int t =(int)f;
+        byte a = 10;
+        byte b = 30;
+        int result =a*b;
 
 
-        System.out.println(t);
+        System.out.println(result);
     }
     
 }
